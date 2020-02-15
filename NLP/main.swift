b9c77ee173a1ce23672d5c01e062531743985bf5
Charles Gunn <cjgunnmail@gmail.com>
2020-02-15 14:09:10 -0500

    quizlet flashcard extraction

--- NLP/main.swift
+++ NLP/main.swift
@@ -1,7 +1,7 @@
-// let st = SpeechText(text: "Lets, like, brings out some, um, big guns")
-// print("\(st.numFillers)")
-// print("\(st.numSlang)")
-// print("\(st.ngrams(n: 2))")
+import Foundation
 
 let qz = Quizlet(url: "https://quizlet.com/190542265/world-war-2-people-flash-cards/")
-print(qz.html)
+for card in qz.cards {
+    print(card.term, card.definition)
+}
+
--- NLP/quizlet.swift
+++ NLP/quizlet.swift
@@ -1,12 +1,32 @@
 import Foundation
+import FoundationNetworking
 import Kanna
+
+class FlashCard {
+    let term : String
+    let definition : String
+    init(term: String, definition: String) {
+        self.term = term
+        self.definition = definition
+    }
+}
 
 class Quizlet {
     var url: URL
-    init(url: String) {
-        self.url = URL(string: url)!
-        if let doc = HTML(url: url, encoding: .utf8) {
+    var cards: [FlashCard]
+    init(url urlString: String) {
+        self.url = URL(string: urlString)!
+        self.cards = []
+        let html = try? String(contentsOf: url)
+        let doc = (try? HTML(html: html!, encoding: .utf8))!
 
+        for node in doc.css(".SetPageTerm-content") {
+            let termDef = node.css(".TermText")
+            if let term = termDef[0].text {
+                if let def = termDef[1].text {
+                    cards.append(FlashCard(term: term, definition: def))
+                }
+            }
         }
     }
 }
