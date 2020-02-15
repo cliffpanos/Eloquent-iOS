6c7cc2f229d164128729b171edc2cf05e7bf7145
Cliff Panos <cliffpanos@icloud.com>
2020-02-14 23:23:48 -0800

    Create Eloquent Xcode Project targeting iOS 13.0. Update gitignore.

---
+++ Eloquent/Eloquent/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Eloquent
+//
+//  Created by Cliff Panos on 2/14/20.
+//  Copyright © 2020 Clifford Panos. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Eloquent/Eloquent/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Eloquent
+//
+//  Created by Cliff Panos on 2/14/20.
+//  Copyright © 2020 Clifford Panos. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
